31da2a21c068c2458e6d35ac3c051233fce4afc2
Grigoriy Sribnyy <ctrhtnysq.afqk@gmail.com>
2024-05-24 23:27:22 +0700

    Сделал страницу импорта. При нажатии кнопки импорта приходит таблица, кроме программ. В них возвращается статус импорта

--- MVC/Controllers/HandbookController.cs
+++ MVC/Controllers/HandbookController.cs
@@ -42,9 +42,99 @@ namespace MVC.Controllers
                     return View("Error");
                 }
             }
-
-            
         }
 
+        public async Task<IActionResult> Faculties()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7023");
+                client.DefaultRequestHeaders.Add("accept", "text/plain");
+                string accessToken = Request.Cookies["accessToken"];
+                string refreshToken = HttpContext.Session.GetString("refreshToken");
+
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+                client.DefaultRequestHeaders.Add("Refresh-token", refreshToken);
+
+                var response = await client.GetAsync("/api/Handbook/Faculties");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var Content = await response.Content.ReadAsStringAsync();
+
+                    var ContentResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<List<FacultyModel>>(Content);
+
+
+                    return PartialView("_FacultiesPartial", ContentResponse);
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: " + response.StatusCode);
+                    ViewData["ErrorMessage"] = response.ToString();
+                    return View("Error");
+                }
+            }
+        }
+
+        public async Task<IActionResult> DocumentTypes()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7023");
+                client.DefaultRequestHeaders.Add("accept", "text/plain");
+                string accessToken = Request.Cookies["accessToken"];
+                string refreshToken = HttpContext.Session.GetString("refreshToken");
+
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+                client.DefaultRequestHeaders.Add("Refresh-token", refreshToken);
+
+                var response = await client.GetAsync("/api/Handbook/DocumentTypes");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var Content = await response.Content.ReadAsStringAsync();
+
+                    var ContentResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<List<EducationDocumentTypeModel>>(Content);
+
+
+                    return PartialView("_DocumentTypesPartial", ContentResponse);
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: " + response.StatusCode);
+                    ViewData["ErrorMessage"] = response.ToString();
+                    return View("Error");
+                }
+            }
+        }
+
+        public async Task<IActionResult> Programs()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7023");
+                client.DefaultRequestHeaders.Add("accept", "text/plain");
+                string accessToken = Request.Cookies["accessToken"];
+                string refreshToken = HttpContext.Session.GetString("refreshToken");
+
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+                client.DefaultRequestHeaders.Add("Refresh-token", refreshToken);
+
+                var response = await client.GetAsync("/api/Handbook/Programs");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var Content = await response.Content.ReadAsStringAsync();
+
+                    return PartialView("_ProgramsPartial", Content);
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: " + response.StatusCode);
+                    ViewData["ErrorMessage"] = response.ToString();
+                    return View("Error");
+                }
+            }
+        }
     }
 }
