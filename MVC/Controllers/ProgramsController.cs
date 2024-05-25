c59b668aa33569b911f931d8ca2915a7e55dd351
Grigoriy Sribnyy <ctrhtnysq.afqk@gmail.com>
2024-05-25 18:52:37 +0700

    Сделал создание очереди. Пока только своей

--- MVC/Controllers/ProgramsController.cs
+++ MVC/Controllers/ProgramsController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using MVC.Models.Handbook;
 using MVC.Models.Programs;
+using Newtonsoft.Json;
 
 namespace MVC.Controllers
 {
@@ -26,6 +28,37 @@ namespace MVC.Controllers
                     var content = await response.Content.ReadAsStringAsync();
                     var contentResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<ProgramPagedListModel>(content);
                     return PartialView("_SearchPrograms", contentResponse);
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: " + response.StatusCode);
+                    ViewData["ErrorMessage"] = response.ToString();
+                    return View("Error");
+                }
+            }
+        }
+
+        public async Task<IActionResult> CreateQuery(List<Guid> programs, Guid userId)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7122/");
+                client.DefaultRequestHeaders.Add("accept", "text/plain");
+                string accessToken = Request.Cookies["accessToken"];
+                string refreshToken = HttpContext.Session.GetString("refreshToken");
+
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+                client.DefaultRequestHeaders.Add("Refresh-token", refreshToken);
+
+                var json = JsonConvert.SerializeObject(programs);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync("/api/Programs/queue", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var contentR = await response.Content.ReadAsStringAsync();
+                    return PartialView("_CreateQuery", "Success");
                 }
                 else
                 {
