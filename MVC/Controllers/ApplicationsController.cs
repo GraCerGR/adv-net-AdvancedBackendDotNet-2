5fc1e63c39f84c300e332a07f8e7662e6875fdc5
Grigoriy Sribnyy <ctrhtnysq.afqk@gmail.com>
2024-05-26 18:34:53 +0700

    Сделал отказ от заявления

--- MVC/Controllers/ApplicationsController.cs
+++ MVC/Controllers/ApplicationsController.cs
@@ -16,10 +16,33 @@ namespace MVC.Controllers
             return View();
         }
 
-/*        public async Task<IActionResult> CreateApplication()
+        public async Task<IActionResult> DeleteManager(Guid ApplicationId)
         {
-            
-        }*/
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7122/");
+                client.DefaultRequestHeaders.Add("accept", "text/plain");
+                string accessToken = Request.Cookies["accessToken"];
+                string refreshToken = Request.Cookies["refreshToken"];
+
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+                client.DefaultRequestHeaders.Add("Refresh-token", refreshToken);
+
+                HttpResponseMessage response = await client.DeleteAsync($"/api/Applications/{ApplicationId}/assign-manager");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return PartialView("AssignManager", content.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: " + response.StatusCode);
+                    ViewData["ErrorMessage"] = response.ToString();
+                    return View("Error");
+                }
+            }
+        }
 
         public async Task<IActionResult> SearchApplication(ApplicationSearchModel searchModel)
         {
